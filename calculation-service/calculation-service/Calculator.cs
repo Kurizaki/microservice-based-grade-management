a9830e13033ab5528a6a8c35d74a2468f3545dca
Timog <110891995+timog06@users.noreply.github.com>
2024-09-17 16:29:26 +0200

    Added Calculator and Model

---
+++ calculation-service/calculation-service/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using calculation_service.Models;
+using System.Collections.Generic;
+
+namespace calculation_service
+{
+    public class Calculator
+    {
+        public double CalculateGrade(List<Grade> grades)
+        {
+            if (grades == null || grades.Count == 0)
+                return 0;
+
+            double weightedSum = 0;
+            double totalWeight = 0;
+
+            foreach (var grade in grades)
+            {
+                if (grade.Score < 1 || grade.Score > 7)
+                {
+                    continue;
+                }
+
+                double weight = grade.Weight ?? 1;
+                if (weight <= 0) weight = 1;
+
+                weightedSum += grade.Score * weight;
+                totalWeight += weight;
+            }
+
+            return totalWeight == 0 ? 0 : weightedSum / totalWeight;
+        }
+
+    }
+}
---
+++ calculation-service/calculation-service/Models/Grade.cs
@@ -0,0 +1,9 @@
+﻿namespace calculation_service.Models
+{
+    public class Grade
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public double? Weight { get; set; }
+    }
+}
