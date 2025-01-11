b4062dec1b5e9e002636e1e94f81976a392ae7a1
Timog <110891995+timog06@users.noreply.github.com>
2025-01-11 16:27:18 +0100

    wtf

--- authentification-service/authentification-service/obj/Debug/net8.0/authentification-service.AssemblyInfo.cs
+++ authentification-service/authentification-service/obj/Debug/net8.0/authentification-service.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("authentification-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e7b1dd70628bb301ec3389d0bea6d9e2bee8b18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fcb3ac0ddb4d629ad7757fc9453250a6cb2be07")]
 [assembly: System.Reflection.AssemblyProductAttribute("authentification-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("authentification-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
